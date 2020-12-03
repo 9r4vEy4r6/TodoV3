813cde7411ba9cafbf792d25ab33ae27963ec06f
PratikGarai <pratikgarai0208@gmail.com>
2020-12-03 12:14:56 +0530

    background research done

---
+++ src/components/TaskItemForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TaskItemForm = (props) =>{
+    return(
+        <div className="task-item-form">
+            Form Widget
+        </div>
+    );
+}
+
+export default TaskItemForm;
--- src/components/TaskItemWidget.js
+++ src/components/TaskItemWidget.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
+import TaskItemForm from './TaskItemForm';
 
 const TaskItemWidget = (props) =>{
+
+    const [form,setForm] = useState(null);
+
+    const renderform = (str) =>{
+        setForm(<TaskItemForm name={str} />)
+        console.log("fired");
+    }
+
     return (
         <div className="task-widget-card">
-            {props.data.name}
+            <button className="task-widget-head" onClick={()=> renderform(props.data.name)}>
+                {props.data.name}
+            </button> 
         </div>
     );
 }

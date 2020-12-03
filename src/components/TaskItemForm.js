16acc108ffeda79c4431536d981e753bb85e7116
PratikGarai <pratikgarai0208@gmail.com>
2020-12-03 20:26:47 +0530

    pausing for now

--- src/components/TaskItemForm.js
+++ src/components/TaskItemForm.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useForm} from 'react-hook-form';
 import '../css/Form.css';
 
 const TaskItemForm = (props) =>{
+
+    const {register, handleSubmit, setValue}  = useForm();
+    
+    useEffect( ()=> {
+        setValue("name", props.task.name);
+        setValue("description", props.task.description);
+    });
+
+    const submitForm = (data) => {
+        data["isComplete"] = false;
+        console.log(data);
+    };
+
     return(
         <div className="task-item-form">
             <hr />
             <button className="remove-button" onClick={props.removeFunc}>Remove</button>
-            <div className="task-name">{props.task.name}</div>
+            <form onSubmit={handleSubmit(submitForm)}>
+                <input type="text" className="task-name"  ref={register} name="name"></input>
+                <input type="text" className="task-description" ref={register} name="description"></input>
+                <select type="select" name="priority" ref={register}>
+                    <option value="1">High</option>
+                    <option value="2">Medium</option>
+                    <option value="3">Low</option>
+                </select>
+                <input type="submit"></input>
+            </form>
         </div>
     );
 }
